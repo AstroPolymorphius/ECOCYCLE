4333d847b24d3287d67a270d09013846856bc6c7
eyram <eyram.fenu@ashesi.edu.gh>
2025-12-02 11:18:51 +0000

    Completed the TransportStage class

--- ECOCYCLE PROJECT/src/TransportationStage.java
+++ ECOCYCLE PROJECT/src/TransportationStage.java
@@ -1,3 +1,20 @@
-public class TransportationStage {
-    
+public class TransportationStage extends LifeCycleStage {
+
+    private ModeOfTransportation mode;
+    private double distance;
+
+
+
+
+
+    public TransportationStage(ModeOfTransportation mode,double distance){
+        super("Transportation");
+        this.mode = mode;
+        this.distance = distance;
+
+        @ Override
+        public double getImpact() {
+            return mode.getEmissionRate() * distance;
+
+    }
 }
