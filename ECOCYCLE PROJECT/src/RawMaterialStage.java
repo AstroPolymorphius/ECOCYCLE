6b13532d230be866c39d2a93978107232ef8894a
James Emmanuel Yeboah-Asiamah <138075694+AstroPolymorphius@users.noreply.github.com>
2025-12-09 21:18:12 +0000

    Refactor RawMaterialStage to manage materials with HashMap
    Refactor RawMaterialStage to use a HashMap for materials and their quantities. Update constructors, getters, and add methods for managing materials.

--- ECOCYCLE PROJECT/src/RawMaterialStage.java
+++ ECOCYCLE PROJECT/src/RawMaterialStage.java
@@ -1,31 +1,42 @@
 public class RawMaterialStage extends LifeCycleStage{
-    private Material material;
-    private double quantity;
+    //Hashmap to store the material and their corresponding amounts/quantity
+    private HashMap<Material, Double> materialsAndTheirQuantity
 
 
-//CONSTRUCTORS
-    public RawMaterialStage(String stageName, double impactValue ,Material material, double quantity){
-        super(stageName, impactValue);
-        this.material = material;
-        this.quantity = quantity;
+//Constructor
+    public RawMaterialStage(String stageName){
+        super(stageName);
+        materialsAndExtractedQuantity = new HashMap<>();
+        
     }
 
 //Getters and Setters
-    public double getQuantity() {
-        return quantity;
+
+    public double getImpact(){
+        for (Map.Entry<Material, Double> entry: materialsAndTheirQuantity.entrySet()){
+            Material material = entry.getKey();
+            double quantity = entry.getValue();
+
+            impactValue += (material.getEmissionFactorPerKg() * quantity);
+        }
+        return impactValue;
     }
 
-    public void setQuantity(double quantity) {
-        this.quantity = quantity;
+    public void addMaterial(Material material, double quantity){
+        //Error handling
+        if (material == null || quantity <= 0){
+            throw new IllegalArgumentException("Invalid material or quantity: material cannot be null and quantity cannot be negative");
+        }
+        if (materialsAndTheirQuantity.containsKey(material)){
+            throw new IllegalArgumentException("Material already exists");
+        }
+        materialsAndTheirQuantity.put(material, quantity)
     }
 
-
-    public Material getMaterial(){
-        return material;
-    }
-
-    public void setMaterial(Material material){
-        this.material = material;
+    public void removeMaterialByName(String materialName){
+        if (materialName == null || materialName.isBlank()){
+            throw new IllegalArgumentException("Invalid material name");
+        }
     }
 
 
