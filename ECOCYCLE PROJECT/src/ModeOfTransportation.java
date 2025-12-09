971cf855811b08b5d2b305e7fd9b331783173900
Sibanda Shaun Mongameli <mongashaun6@gmail.com>
2025-12-09 22:56:06 +0000

    Refactor ModeOfTransportation class
    Removed unused fields and updated constructor. Enhanced error handling in setters and added toString, equals, and hashCode methods.

--- ECOCYCLE PROJECT/src/ModeOfTransportation.java
+++ ECOCYCLE PROJECT/src/ModeOfTransportation.java
@@ -2,22 +2,12 @@ public class ModeOfTransportation {
     //Encapsulating the data fields
     private String name;
     private double emissionRate;
-    private double distanceCovered;
-    private double impact;
-
-
-
     
     //parameterized constructor for the mode of transportation
-    public ModeOfTransportation(String name, double emissionRate, double distanceCovered){
-        this.name = name;
-        this.emissionRate = emissionRate;
-        this.distanceCovered = distanceCovered;
-        impact = emissionRate * distanceCovered;
-    }
-
-
-
+    public ModeOfTransportation(String name, double emissionRate){
+        this.setName(name);
+        this.setEmissionRate(emissionRate);}
+        
     //Getters
     public String getName(){
         return name;
@@ -26,45 +16,40 @@ public class ModeOfTransportation {
     public double getEmissionRate(){
         return emissionRate;
     }
-    public double getDistanceCovered(){
-        return distanceCovered;
-    }
-
-    public double getImpact(){
-        return impact;
-    }
-
-
-
+    
     //Setters
     public void setName(String name){
         //Error handling
         if (name == null || name.isBlank()){
             throw new IllegalArgumentException("Mode of Transportation's name cannot be left empty or blank");
         }
-        else{
-            this.name = name;
-        }
+        this.name = name;
 
     }
 
     public void setEmissionRate(double emissionRate){
         //Error handling
-        if (emissionRate <= 100){
+        if (emissionRate <= 0){
             throw new IllegalArgumentException("The mode of transportation's emission rate cannot be 0 or negative");
         }
-        else{
-            this.emissionRate = emissionRate;
-        }
+        this.emissionRate = emissionRate;
     }
 
-    public void setDistanceCovered(double distanceCovered){
-        if (distanceCovered <= 0){
-            throw new IllegalArgumentException("Distance covered cannot be negative or 0");
-        }
-        else{
-            this.distanceCovered = distanceCovered;
-        }
+    @Override
+    public String toString() {
+        return "Mode of Transportation Name: " + name +
+               ", Emission Rate: " + emissionRate;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ModeOfTransportation that = (ModeOfTransportation) obj;
+        return name.equals(that.name);
+    }
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 
 
