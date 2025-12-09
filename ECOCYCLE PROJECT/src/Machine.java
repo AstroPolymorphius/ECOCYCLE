98f0f305e217d3594d1e756699e4b8e777b135f6
Sibanda Shaun Mongameli <mongashaun6@gmail.com>
2025-12-09 22:53:33 +0000

    Refactor Machine constructor and add setters

--- ECOCYCLE PROJECT/src/Machine.java
+++ ECOCYCLE PROJECT/src/Machine.java
@@ -4,14 +4,10 @@ public class Machine {
     private double energyConsumption;
 
 
-
-
     public Machine(String name, double energyConsumption) {
-        this.name = name;
-        this.energyConsumption = energyConsumption;
+        this.setName(name);
+        this.setEnergyConsumption(energyConsumption);
     }
-
-
 
     public String getName() {
         return name;
@@ -20,4 +16,33 @@ public class Machine {
     public double getEnergyConsumption() {
         return energyConsumption;
     }
+    public void setEnergyConsumption(double energyConsumption) {
+        if (energyConsumption < 0) {
+            throw new IllegalArgumentException("Energy consumption cannot be negative");
+        }
+        this.energyConsumption = energyConsumption;
+    }
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Machine Name: " + name +
+               ", Energy Consumption: " + energyConsumption;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Machine that = (Machine) obj;
+        return name.equals(that.name);
+    }
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+    
 }
