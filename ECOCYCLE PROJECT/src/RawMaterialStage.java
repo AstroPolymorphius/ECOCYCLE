2c9421f59b30b75aa47ec016bf790230469ea231
Sibanda Shaun Mongameli <mongashaun6@gmail.com>
2025-12-09 17:30:54 +0000

    Extend RawMaterialStage to include material and quantity

--- ECOCYCLE PROJECT/src/RawMaterialStage.java
+++ ECOCYCLE PROJECT/src/RawMaterialStage.java
@@ -1,3 +1,32 @@
-public class RawMaterialStage {
-    
+public class RawMaterialStage extends LifeCycleStage{
+    private Material material;
+    private double quantity;
+
+
+//CONSTRUCTORS
+    public RawMaterialStage(String stageName, double impactValue ,Material material, double quantity){
+        super(stageName, impactValue);
+        this.material = material;
+        this.quantity = quantity;
+    }
+
+//Getters and Setters
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public Material getMaterial(){
+        return material;
+    }
+
+    public void setMaterial(Material material){
+        this.material = material;
+    }
+
+
 }
