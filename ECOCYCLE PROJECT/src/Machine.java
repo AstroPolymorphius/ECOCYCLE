949421cef825e4a378f1a0ab153a94a42c990200
Sibanda Shaun Mongameli <shaun.sibanda@ashesi.edu.gh>
2025-12-09 19:42:34 +0000

    Fixed accidental nested git repository

--- ECOCYCLE PROJECT/src/Machine.java
+++ ECOCYCLE PROJECT/src/Machine.java
@@ -1,3 +1,23 @@
 public class Machine {
-    
+
+    private String name;
+    private double energyConsumption;
+
+
+
+
+    public Machine(String name, double energyConsumption) {
+        this.name = name;
+        this.energyConsumption = energyConsumption;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getEnergyConsumption() {
+        return energyConsumption;
+    }
 }
