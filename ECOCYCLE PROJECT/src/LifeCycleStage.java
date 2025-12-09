3e0251be56bb8c4d35d29e90440c98d700dc7f36
Sibanda Shaun Mongameli <mongashaun6@gmail.com>
2025-12-09 22:53:01 +0000

    Make LifeCycleStage abstract and modify constructors
    Refactor LifeCycleStage class to be abstract and remove impactValue.

--- ECOCYCLE PROJECT/src/LifeCycleStage.java
+++ ECOCYCLE PROJECT/src/LifeCycleStage.java
@@ -1,14 +1,11 @@
-public class LifeCycleStage {
+public abstract class LifeCycleStage {
     private String stageName;
-    private double impactValue;
-
-
 
 //Constructors
-    public LifeCycleStage(String stageName, double impactValue) {
-        this.stageName = stageName;
-        this.impactValue = impactValue;
+    public LifeCycleStage(String stageName) {
+        this.setStageName(stageName);
     }
+    public abstract double getImpactValue();
 
 
 //Getters and Setters
@@ -16,24 +13,17 @@ public class LifeCycleStage {
         return stageName;
     }
 
-    public double getStageImpactValue() {
-        return impactValue;
-    }
-
-
     public void setStageName(String stageName) {
+        if(stageName == null || stageName.trim().isEmpty()){
+            throw new IllegalArgumentException("Stage name cannot be null or empty");
+        }
         this.stageName = stageName;
     }
-
-    public void setImpactValue(double impactValue) {
-        this.impactValue = impactValue;
-    }
-
 
 //Displaying the information
     @Override
     public String toString() {
-        return "\nStage Name: " + stageName + "\nValue of Impact: " + impactValue;
+        return "\nStage Name: " + stageName + "\nImpact Value: " + this.getImpactValue();
     }
 
 
